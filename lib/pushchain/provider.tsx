905865a1a2f732d85fd1bc0e299f0514de843854
penguinpecker <penguinpecker1@gmail.com>
2026-04-10 22:43:27 +0530

    revert: remove broken chains config from wallet provider

--- lib/pushchain/provider.tsx
+++ lib/pushchain/provider.tsx
@@ -53,9 +53,6 @@ export function PushChainWalletProvider({ children, network = "testnet" }: Props
       google: true,
       wallet: {
         enabled: true,
-        chains: [
-          PushUI.CONSTANTS.CHAIN.PUSH_TESTNET_DONUT,
-        ],
       },
       appPreview: true,
     },
